82f3b1e12ade4a3fa24a56f5ba0118bfa06fd1f8
dineshpatil9006 <dineshpatil9006@gmail.com>
2024-08-02 14:32:48 +0530

    edit and remove functionality added

--- toDoList/script.js
+++ toDoList/script.js
@@ -1,29 +1,50 @@
 const inputBox = document.getElementById("inputBox");
 const addBtn = document.getElementById("addBtn");
 const todoList = document.getElementById("todoList");
+let editTodo = null;
 
 const addTodo = () => {
   const inputText = inputBox.value.trim();
   if (inputText.length == 0) {
     alert("You must write something in your todo");
+    return false;
+  }
+  if (addBtn.value === "Edit") {
+    editTodo.target.previousElementSibling.innerHTML = inputText;
+    addBtn.value = "Add";
+    inputBox.value = "";
   } else {
     let li = document.createElement("li");
     let p = document.createElement("p");
     p.innerHTML = inputText;
     li.appendChild(p);
 
-    const deleteBtn = document.createElement('button');
-    deleteBtn.innerHTML = "Remove";
-    deleteBtn.classList.add('btn', 'deleteBtn');
-    li.appendChild(deleteBtn);
-    
-    const editBtn = document.createElement('button');
+    const editBtn = document.createElement("button");
     editBtn.innerHTML = "Edit";
-    editBtn.classList.add('btn', 'editBtn');
+    editBtn.classList.add("btn", "editBtn");
     li.appendChild(editBtn);
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerHTML = "Remove";
+    deleteBtn.classList.add("btn", "deleteBtn");
+    li.appendChild(deleteBtn);
 
     todoList.appendChild(li);
     inputBox.value = "";
   }
 };
+
+const updateTodo = (e) => {
+  if (e.target.innerHTML === "Remove") {
+    todoList.removeChild(e.target.parentElement);
+  }
+  if (e.target.innerHTML === "Edit") {
+    inputBox.value = e.target.previousElementSibling.innerHTML;
+    inputBox.focus();
+    addBtn.value = "Edit";
+    editTodo = e;
+  }
+};
+
 addBtn.addEventListener("click", addTodo);
+todoList.addEventListener("click", updateTodo);
