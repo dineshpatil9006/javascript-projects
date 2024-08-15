d94191ebdb0bdd0f7644e797f5c06bd3ec8f1cba
dineshpatil9006 <dineshpatil9006@gmail.com>
2024-08-15 11:09:35 +0530

    scored showing functionality added

--- quizApp/script.js
+++ quizApp/script.js
@@ -2,8 +2,10 @@ const container = document.querySelector(".container");
 const questionBox = document.querySelector(".question");
 const choicesBox = document.querySelector(".choices");
 const nextBtn = document.querySelector(".nextBtn");
+const scoreCard = document.querySelector(".scoreCard");
 
 let curruntQuestionIndex = 0;
+let score = 0;
 
 const quiz = [
   {
@@ -28,34 +30,66 @@ const quiz = [
   },
 ];
 
-let showQuestions = () =>{
-    const questionDetails = quiz[curruntQuestionIndex];
-    questionBox.textContent = questionDetails.question;
+let showQuestions = () => {
+  const questionDetails = quiz[curruntQuestionIndex];
+  questionBox.textContent = questionDetails.question;
 
-    choicesBox.textContent = "";
-    
-    for(let i = 0; i<questionDetails.options.length; i++){
-        const currentChoice = questionDetails.options[i];
-        const choiceDiv = document.createElement('div');
-        choiceDiv.textContent = currentChoice;
-        choiceDiv.classList.add('choice');
-        choicesBox.appendChild(choiceDiv);
+  choicesBox.textContent = "";
 
-        choiceDiv.addEventListener('click',()=>{
-          if(choiceDiv.classList.contains('selected')){
-            choiceDiv.classList.remove('selected');
-          }else{
-            choiceDiv.classList.add('selected');
-          }
-        });
-    }
+  for (let i = 0; i < questionDetails.options.length; i++) {
+    const currentChoice = questionDetails.options[i];
+    const choiceDiv = document.createElement("div");
+    choiceDiv.textContent = currentChoice;
+    choiceDiv.classList.add("choice");
+    choicesBox.appendChild(choiceDiv);
 
-}
+    choiceDiv.addEventListener("click", () => {
+      if (choiceDiv.classList.contains("selected")) {
+        choiceDiv.classList.remove("selected");
+      } else {
+        choiceDiv.classList.add("selected");
+      }
+    });
+  }
+};
+
+const checkAnswer = () => {
+  const selectedChoice = document.querySelector(".choice.selected");
+  if (selectedChoice.textContent === quiz[curruntQuestionIndex].answer) {
+    alert("correct answer");
+    score++;
+  } else {
+    alert("wrong answer");
+  }
+
+  curruntQuestionIndex++;
+  if (curruntQuestionIndex < quiz.length) {
+    showQuestions();
+  } else {
+    showScore();
+  }
+};
+
+const showScore = () => {
+  questionBox.textContent = "";
+  choicesBox.textContent = "";
+  scoreCard.textContent = `You scored ${score} out of ${quiz.length}!`;
+  nextBtn.textContent = "Play Again";
+  nextBtn.addEventListener('click',()=>{
+    curruntQuestionIndex = 0;
+    nextBtn.textContent = "Next";
+    scoreCard.textContent = "";
+    showQuestions();
+  });
+};
 
 showQuestions();
-nextBtn.addEventListener('click', ()=>{
-    if(curruntQuestionIndex<quiz.length){
-        curruntQuestionIndex++;
-        showQuestions();
-    }
+nextBtn.addEventListener("click", () => {
+  const selectedChoice = document.querySelector(".choice.selected");
+  if (!selectedChoice && nextBtn.textContent == "Next") {
+    alert("Select Your Answer");
+    return;
+  } else {
+    checkAnswer();
+  }
 });
